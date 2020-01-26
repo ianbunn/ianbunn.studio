fadca326aff668ddc9c1898ac94685cb7c38a43e
ianbunn <ianrichardbunn@gmail.com>
2020-01-26 15:00:46 -0600

    initial portfolio template

---
+++ src/views/components/Copyright/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import Typography from '@material-ui/core/Typography';
+import Link from '@material-ui/core/Link';
+
+export default class Copyright extends Component {
+    render() {
+        return (
+            <Typography variant="body2" color="textSecondary">
+                {'Made by '}
+                <Link color="inherit" href="http://ianbunn.studio/">
+                    ianbunn.studio
+                </Link>{' '}
+                {' © '}
+                {new Date().getFullYear()}
+            </Typography>
+        )
+    }
+}
---
+++ src/views/components/Quote/index.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react'
+
+const SHEET_ID = process.env.REACT_APP_GOOGLE_SHEET_ID
+const ACCESS_TOKEN = process.env.REACT_APP_ACCESS_TOKEN
+
+let row, element;
+export default class Quote extends Component {
+    state = {}
+
+    componentDidMount() {
+        this.getSheetValues();
+    }
+
+    checkForRowNotOne = (row) => {
+        if (row === 1) {
+            return row + 1
+        } else {
+            return row
+        }
+    }
+    getRandomRow = () => {
+        row = Math.floor(Math.random() * Math.floor(1477))
+        this.checkForRowNotOne(row)
+        return row
+    }
+    
+    getSheetValues = async () => {
+        try {
+            this.getRandomRow();
+            const request = await fetch(`https://sheets.googleapis.com/v4/spreadsheets/${SHEET_ID}/values/A${row}:B${row}`,
+                {
+                    headers: {
+                        "Content-Type": "application/json",
+                        Authorization: `Bearer ${ACCESS_TOKEN}`
+                    }
+                });
+            const data = await request.json();
+            element = `"${data.values[0][1]}" by ${data.values[0][0]}`
+            this.setState({
+                Quote: element
+            })
+            console.log(element)
+        } catch(err) {
+            console.log(err)
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <h4>{this.state.Quote}</h4>
+            </div>
+        )
+    }
+    
+}
