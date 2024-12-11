9d0ef5512c0933b7b851bee3eddc9b652923091d
ian b <ianrichardbunn@gmail.com>
2024-12-10 22:14:49 -0600

    Rebrand site completely, initial revamp and debugging email

--- src/Routes.tsx
+++
@@ -1,9 +0,0 @@
-import { createBrowserRouter } from 'react-router-dom';
-import { Landing } from "./pages";
-
-export const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <Landing />,
-    },
-]);
